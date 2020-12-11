5decf8a4aef42ec532d760ed0a081fcccd6ac5ed
Karim <karimalaa12341@outlook.com>
2020-12-11 13:38:23 +0200

    commit dlls

---
+++ ProductCatalog_API_CeanArch/CleanArch.Repositories/Repository/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Repositories.Repository
+{
+    interface IRepository
+    {
+        List<T> GetAll<T>(bool withTracking = false) where T : class;
+
+        List<T> GetAllWithPaging<T>(int skip, int take, bool withTracking = false) where T : class;
+
+        IQueryable<T> GetAllWhereQ<T>(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes) where T : class;
+
+        void Add<T>(T entity) where T : class;
+
+        void Update<T>(T entity) where T : class;
+
+        void Remove<T>(T entity) where T : class;
+
+        int Count<T>() where T : class;
+
+        bool SaveChangesAsync();
+    }
+}
---
+++ ProductCatalog_API_CeanArch/CleanArch.Repositories/Repository/Repository.cs
@@ -0,0 +1,110 @@
+﻿using CleanArc.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Repositories.Repository
+{
+    public class Repository : IRepository
+    {
+        CleanArchDBContext _context;
+
+        public Repository(CleanArchDBContext context)
+        {
+            _context = context;
+        }
+
+        #region Private Methods
+
+        private IQueryable<T> InsializeQuery<T>(params Expression<Func<T, object>>[] includes) where T : class
+        {
+            var query = _context.Set<T>().AsQueryable();
+            if (includes.Any())
+            {
+                foreach (var include in includes)
+                {
+                    query = query.Include(include);
+                }
+            }
+            return query;
+        }
+
+        #endregion
+
+        #region Add
+
+        public void Add<T>(T entity) where T : class
+        {
+            _context.Add(entity);
+        }
+
+        #endregion
+
+        #region Count
+
+        public int Count<T>() where T : class
+        {
+            return _context.Set<T>().Count();
+        }
+
+        #endregion
+
+        #region Get Methods
+
+        public IQueryable<T> GetAllWhereQ<T>(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includes) where T : class
+        {
+            return InsializeQuery<T>(includes).Where(predicate);
+        }
+
+        public List<T> GetAllWithPaging<T>(int skip, int take, bool withTracking) where T : class
+        {
+            if (withTracking)
+                return InsializeQuery<T>().Skip(skip).Take(take).ToList();
+            else
+                return InsializeQuery<T>().Skip(skip).Take(take).AsNoTracking().ToList();
+        }
+
+        public List<T> GetAll<T>(bool withTracking = true) where T : class
+        {
+            if (withTracking)
+                return InsializeQuery<T>().ToList();
+            else
+                return InsializeQuery<T>().AsNoTracking().ToList();
+        }
+
+        #endregion
+
+        #region Remove
+
+        public void Remove<T>(T entity) where T : class
+        {
+            _context.Remove(entity);
+        }
+
+        #endregion
+
+        #region Update
+
+        public void Update<T>(T entity) where T : class
+        {
+            _context.Attach(entity);
+            _context.Update<T>(entity);
+        }
+
+        #endregion
+
+        #region Save Changes
+
+        public bool SaveChangesAsync()
+        {
+            return _context.SaveChanges() > 0;
+        }
+
+        #endregion
+
+    }
+}
